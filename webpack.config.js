2d9b453c089ed562748b8c48f1c1dcc077eaceaa
Carla <ccairns1999@gmail.com>
2021-06-19 21:55:40 -0500

    loading data

--- webpack.config.js
+++ webpack.config.js
@@ -16,12 +16,24 @@ const path = require('path');
          test: /\.(png|svg|jpg|jpeg|gif)$/i,
          type: 'asset/resource',
        },
+       {
+         test: /\.(woff|woff2|eot|ttf|otf)$/i,
+         type: 'asset/resource',
+       },
 
       {
 
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        test: /\.(csv|tsv)$/i,
 
-        type: 'asset/resource',
+        use: ['csv-loader'],
+
+      },
+
+      {
+
+        test: /\.xml$/i,
+
+        use: ['xml-loader'],
 
       },
      ],
