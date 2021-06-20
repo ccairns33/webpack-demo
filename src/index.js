cb43dc9f28b90001cecadff4cbb86d24f3983653
Carla <ccairns1999@gmail.com>
2021-06-19 20:15:10 -0500

    init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-import './style.css';
-
- function component() {
-   const element = document.createElement('div');
-
-   // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  element.classList.add('hello');
-
-   return element;
- }
-
- document.body.appendChild(component());
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
